2d695dec81148c9e8e5713ff01c0b64ef0e97859
wenqi <WHan@smail.pembinatrails.ca>
2024-11-04 14:01:51 -0600

    new script added

--- script1.sh
+++ script1.sh
@@ -17,3 +17,15 @@ check_upgrades() {
     echo "Checking for system upgrades..."
     sudo apt list --upgradable
 }
+
+# Function to check if a directory exist and if not, make one
+DIRECTORY="schoolwork"
+if [ -d "$DIRECTORY" ]; then
+    echo "Directory '$DIRECTORY' already exists."
+else
+    echo "Directory '$DIRECTORY' does not exist. would you like to create it now? (y/n)"
+if [ "create it now" == "y" ]
+mkdir "$DIRECTORY"
+echo "Directory '$DIRECTORY' created."
+fi
+
